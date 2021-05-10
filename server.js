5128b4fc18c67be238253aecc93f544851b72475
David Bushard <davidbushard@gmail.com>
2021-05-10 11:31:26 -0700

    trying to connect AtlasDB 2

--- server.js
+++ server.js
@@ -15,12 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://DavidBushard:password123@cluster0.qszqz.mongodb.net/budget?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
